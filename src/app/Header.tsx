6a68e758dd80b449538f27cdbf21fd5b6f76329f
takuabonn <k.takumi19980322@gmail.com>
2024-02-10 19:58:10 +0900

    firebase設定to

---
+++ src/app/Header.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { signInWithGoogle, signOut } from "@/lib/firebases/auth";
+import { getCurrentUser } from "@/lib/firebases/server";
+import { UserRecord } from "firebase-admin/auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const Header = () => {
+  const router = useRouter();
+  
+
+  const handleSignIn = async () => {
+    console.log("lo")
+    const isOk = await signInWithGoogle();
+
+    if (isOk) router.push("/");
+  };
+
+  const handleSignOut = async () => {
+    const isOk = await signOut();
+
+    if (isOk) router.push("/");
+  };
+  
+  return (
+    <>
+     <div className=" flex justify-between content-center items-center w-full h-12 bg-green-400 text-white mb-3">
+        <div className="pl-5">
+        <h1> JSONDB-DESIGNER</h1>
+        </div>
+        <div className="pr-5">
+        <button className="inline-block px-3" onClick={handleSignIn}>Login</button>
+        <button className="inline-block px-3" onClick={handleSignOut}>Logout</button>
+        </div>
+        </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { Editor } from "@monaco-editor/react";
 import { ERDiagram } from "./feature/ErDiagram";
+import { useState } from "react";
+import { getCurrentUser } from "@/lib/firebases/server";
 
-export default function Home() {
+
+export default async function Home() {
+  const currentUser = await getCurrentUser();
+  
   return (
     <>
-    <ERDiagram/>
+      <div>{currentUser ? currentUser.displayName : "ゲスト"}</div>
     </>
   );
 }
