3a0a848ac3da0b0e666b5272a8be99fde01c9f20
takuabonn <k.takumi19980322@gmail.com>
2024-02-11 11:14:44 +0900

    不要コード削除

--- src/lib/firebases/server.ts
+++ src/lib/firebases/server.ts
@@ -4,15 +4,6 @@ import { initializeApp, cert, getApps, App } from 'firebase-admin/app';
 import { SessionCookieOptions, getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
-
-// if (!getApps()?.length) {
-//   const firebaseApp = initializeApp({
-//     credential: cert(
-//       // 環境変数から認証情報を取得
-//       JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
-//     ),
-//   });
-// }
 // export const runtime = 'edge';
 
 export const firebaseApp =
