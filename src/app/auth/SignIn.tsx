5ca6f4cd5c08aa5af0d614f6166ecf85a506ed55
takuabonn <k.takumi19980322@gmail.com>
2024-02-17 23:20:37 +0900

    topページのレイアウト変更

--- src/app/auth/SignIn.tsx
+++ src/app/auth/SignIn.tsx
@@ -1,20 +1,22 @@
-"use client"
+"use client";
 import { signInWithGoogle } from "@/lib/firebases/auth";
 import { useRouter } from "next/navigation";
-import React from "react"
+import React from "react";
 
 export const SignIn = () => {
-    const router = useRouter();
-  
+  const router = useRouter();
 
   const handleSignIn = async () => {
     const isOk = await signInWithGoogle();
 
     if (isOk) router.push("/project/previewMode");
   };
-    return (
-        <button className="w-44 h-16 mt-16 bg-white hover:bg-gray-400/50 rounded-lg text-2xl text-gray-400 absolute left-44" onClick={handleSignIn}>
-        Try it
-        </button>
-    )
-}
+  return (
+    <button
+      className="w-44 h-16 mt-24 bg-green-400 hover:bg-green-400/50 rounded-lg text-2xl text-white absolute mx-auto"
+      onClick={handleSignIn}
+    >
+      Try it
+    </button>
+  );
+};
